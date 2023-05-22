9e1982657582da45d982b4ab05056063704d2a7d
tusharsawantt <sawanttushar439@gmail.com>
2023-05-22 19:51:02 +0530

    first commit

---
+++ js/common.js
@@ -0,0 +1,21 @@
+// document ready function
+$(document).ready(function () {
+
+  $('.owl-carousel, .gallery-center-slider').owlCarousel({
+    stagePadding:0,
+    loop:true,
+    items:3,
+    // lazyLoad: true,
+    autoplay: true,
+    center: true,
+  
+    // autoplaySpeed: 2000,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true
+});
+
+
+
+
+
+});
