2bbd7ef1ddb2f3f13b44ae01f10b879098736e3a
tusharsawantt <sawanttushar439@gmail.com>
2023-05-24 12:11:39 +0530

    changes done

--- js/common.js
+++ js/common.js
@@ -5,17 +5,28 @@ $(document).ready(function () {
     stagePadding:0,
     loop:true,
     items:3,
-    // lazyLoad: true,
+    lazyLoad: true,
     autoplay: true,
     center: true,
-  
-    // autoplaySpeed: 2000,
+    autoplaySpeed: 2000,
     autoplayTimeout: 5000,
-    autoplayHoverPause: true
+    autoplayHoverPause: true,
+    responsive:{
+      700:{
+      items:1,
+      center:false,
+      }
+  }
+});
+
+$(".contain .prev").click(function(){
+  $(".owl-prev").trigger("click");
+});
+$(".contain .next").click(function(){
+  $(".owl-next").trigger("click");
 });
 
 
-
-
+// $(".speak-slider .next").trigger(".owl-next")
 
 });
