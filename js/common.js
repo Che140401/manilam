7fd5efe3f678220f7ebd454286d665774714a4cd
tusharsawantt <sawanttushar439@gmail.com>
2023-05-25 14:48:22 +0530

    tushar commit

--- js/common.js
+++ js/common.js
@@ -1,23 +1,61 @@
 // document ready function
 $(document).ready(function () {
 
-  $('.owl-carousel, .gallery-center-slider').owlCarousel({
+  $('.owl-carousel, .gallery-center-slider ').owlCarousel({
     stagePadding:0,
     loop:true,
+    nav:true,
     items:3,
     lazyLoad: true,
     autoplay: true,
     center: true,
-    autoplaySpeed: 2000,
-    autoplayTimeout: 5000,
+    // autoplaySpeed: 1000,
+    // autoplayTimeout: 1000,
     autoplayHoverPause: true,
-    responsive:{
-      700:{
-      items:1,
-      center:false,
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      600: {
+        items: 3
+      },
+  
+      1024: {
+        items: 3
+      },
+  
+      1366: {
+        items: 3
       }
-  }
+    }
 });
+
+// $('.gallery-center-slider').slick({
+//   centerMode: true,
+//   centerPadding: '100px',
+//   slidesToShow: 3,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: false,
+//         centerPadding: '40px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// });
 
 $(".contain .prev").click(function(){
   $(".owl-prev").trigger("click");
